2fdd2d8978cb605341fe15b9d883ab3dd1fd4fce
Varshith Julakanti <varshithjulakanti@gmail.com>
2026-06-24 18:40:39 +0530

    Update MyServiceTest.java

--- week-2/Mockito exercises/Exercise2_VerifyingInteractions/VerifyingInteractions/src/test/java/MyServiceTest.java
+++ week-2/Mockito exercises/Exercise2_VerifyingInteractions/VerifyingInteractions/src/test/java/MyServiceTest.java
@@ -7,19 +7,14 @@ public class MyServiceTest {
 
     @Test
     public void testVerifyInteraction() {
-
-        // Create Mock Object
         ExternalApi mockApi =
                 Mockito.mock(ExternalApi.class);
 
-        // Inject Mock
         MyService service =
                 new MyService(mockApi);
 
-        // Call Method
         service.fetchData();
 
-        // Verify Interaction
         verify(mockApi).getData();
     }
 }
