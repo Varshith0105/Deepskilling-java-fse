23213f833e33bf87d7cb7f92c9738bd5e5671202
Varshith Julakanti <varshithjulakanti@gmail.com>
2026-06-24 18:39:41 +0530

    Update MyServiceTest.java

--- week-2/Mockito exercises/Exercise1_MockingandStubbing/Exercise1_MockingandStubbing/src/test/java/MyServiceTest.java
+++ week-2/Mockito exercises/Exercise1_MockingandStubbing/Exercise1_MockingandStubbing/src/test/java/MyServiceTest.java
@@ -8,24 +8,14 @@ public class MyServiceTest {
 
     @Test
     public void testExternalApi() {
-
-        // Create Mock Object
         ExternalApi mockApi =
                 Mockito.mock(ExternalApi.class);
-
-        // Stub Method
         when(mockApi.getData())
                 .thenReturn("Mock Data");
-
-        // Inject Mock
         MyService service =
                 new MyService(mockApi);
-
-        // Call Method
         String result =
                 service.fetchData();
-
-        // Verify Result
         assertEquals(
                 "Mock Data",
                 result
