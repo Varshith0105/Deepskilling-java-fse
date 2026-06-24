05c6839bde46132cd6879047e664a1b97225a647
Varshith0105 <varshithjulakanti@gmail.com>
2026-06-24 15:11:10 +0530

    Added Week-2 JUnit and Mockito exercises

---
+++ Mockito exercises/Exercise2_VerifyingInteractions/VerifyingInteractions/src/test/java/MyServiceTest.java
@@ -0,0 +1,25 @@
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class MyServiceTest {
+
+    @Test
+    public void testVerifyInteraction() {
+
+        // Create Mock Object
+        ExternalApi mockApi =
+                Mockito.mock(ExternalApi.class);
+
+        // Inject Mock
+        MyService service =
+                new MyService(mockApi);
+
+        // Call Method
+        service.fetchData();
+
+        // Verify Interaction
+        verify(mockApi).getData();
+    }
+}
